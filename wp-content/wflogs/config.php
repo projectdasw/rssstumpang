2c16e4c8bf6b61ce85774e8aef6e9f49a7b0b4c5
projectdasw <project.dasw@gmail.com>
2025-10-19 11:09:11 +0700

    Setup Awal sistem WordPress

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"Zg_Ye8gcvy!n nn;JIO|nhmz*9mm7AV3Yn3DV5#T,bE&d[0A X/Q.m2^k.?Q5|cx";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1522;}
